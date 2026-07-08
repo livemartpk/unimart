c3c512213599f10e3f5445fd38e5d023d091d7a3
livemartpk <chakno26bc@gmail.com>
2026-07-08 15:23:20 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import LoadingLogo from './components/LoadingLogo.jsx'
 import './styles/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<LoadingLogo />}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 )
