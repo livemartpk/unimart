c8939f85d762a3798d5936fc9642f442e52056ae
livemartpk <chakno26bc@gmail.com>
2026-07-18 11:36:45 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import LoadingLogo from './components/LoadingLogo.jsx'
 import './styles/theme.css'
+import './styles/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
